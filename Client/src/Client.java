6df58c65455567a7f2b40db920e1d9744b3fffb9
kirill-gvozdev <kirils.gvozdevs@icloud.com>
2020-10-26 21:37:08 +0200

    Client module

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -3,7 +3,6 @@ import java.net.Socket;
 
 public class Client {
 
-
     private static final int PORT = 4999;
     private static final String HOST = "localhost";
     private static Socket clientSocket; //сокет для соединения
